922a0e496b33e22da145237d0c80b24f7d8eccbd
dscifo <diego.scifo@globallogic.com>
2022-08-29 09:10:17 -0300

    First commit

---
+++ Chapter 02/PerformanceExample1/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        PrimeNumbers primeNumbers = new PrimeNumbers();
+        Integer max = Integer.parseInt(args[0]);
+        primeNumbers.generateNumbers(max);
+    }
+
+}
